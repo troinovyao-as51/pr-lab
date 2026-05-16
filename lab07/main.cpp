18e5fbe971d2f6a7a0b46f1cafe4b65ab0114842
troinovyao-as51 <troinov.ya.o.-as51@edu.kpi.ua>
2026-05-16 20:26:34 +0300

    Update main.cpp

--- lab07/main.cpp
+++ lab07/main.cpp
@@ -1,11 +1,9 @@
 Node<int>* create_linked_list(std::vector<int>& values) {
-    if (values.empty()) return nullptr; // Если массив пуст, возвращаем nullptr
+    if (values.empty()) return nullptr; 
 
-    // Создаем голову списка с первым элементом
     Node<int>* head = new Node<int>(values[0]);
     Node<int>* current = head;
 
-    // Итерируемся по остальным элементам и связываем их
     for (size_t i = 1; i < values.size(); ++i) {
         current->next = new Node<int>(values[i]);
         current = current->next;
